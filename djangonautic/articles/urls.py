10b6aba36bee4d418791ba3dbbddab2c2ab094b3
orangeztorm <taiwokenny45@gmail.com>
2021-01-08 20:24:33 +0100

    Initial commit

---
+++ djangonautic/articles/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+app_name = 'articles'
+
+urlpatterns = [
+    path('', views.article_list, name='list'),
+    path('<slug:slug>/', views.article_detail, name='detail')
+    # path(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
+]
