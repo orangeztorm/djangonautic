bbfad89831bc14a3a003d80ee2595c7f4b77cc78
orangeztorm <taiwokenny45@gmail.com>
2021-01-10 16:17:40 +0100

    redirecting after logging in

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from . import forms
 from .models import Article
 
 
@@ -19,4 +20,11 @@ def article_detail(request, slug):
 
 @login_required(login_url="/accounts/login/")
 def article_create(request):
-    return render(request, 'articles/article_create.html', )
+    if request.method == 'POST':
+        form = forms.createArticle(request.POST, request.FILES)
+        if form.is_valid():
+            # save article to db
+            return redirect('articles:list')
+    else:
+        form = forms.createArticle()
+    return render(request, 'articles/article_create.html', {'form': form})
