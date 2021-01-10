fbc7bcab5c36f53373b1d4e6672bbb4edae674b5
orangeztorm <taiwokenny45@gmail.com>
2021-01-10 14:58:53 +0100

    loggin users in

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 
@@ -28,3 +28,9 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('articles:list')
