4ba3aff47577f9d45a8f5270ae54ded3b47ccecc
orangeztorm <taiwokenny45@gmail.com>
2021-01-10 15:25:34 +0100

    loggin users in

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from .models import Article
@@ -14,3 +15,8 @@ def article_detail(request, slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', {'article': article})
+
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    return render(request, 'articles/article_create.html', )
